50cf8d6043b3511cffb6324c2b3162d0cfc19e97
Aleksej <git@aleksejdix.com>
2020-05-27 22:38:18 +0200

    Revert "Update database config"
    This reverts commit d1a659b320be48ccf20ed465c5bddf23bc7d3f6d.

--- config/database.js
+++ config/database.js
@@ -1,18 +1,15 @@
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
-  "connections": {
-    "default": {
-      "connector": "bookshelf",
-      "settings": {
-        "client": "postgres",
-        "host": "${process.env.DATABASE_HOST}",
-        "port": "${process.env.DATABASE_PORT}",
-        "database": "${process.env.DATABASE_NAME}",
-        "username": "${process.env.DATABASE_USERNAME}",
-        "password": "${process.env.DATABASE_PASSWORD}",
-        "ssl": { "rejectUnauthorized": false }
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      "options": {}
-    }
+      options: {
+        useNullAsDefault: true,
+      },
+    },
   },
 });
