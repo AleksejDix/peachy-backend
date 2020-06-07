41329e21a846972002a392b7d9467bf0d1334804
Aleksej <git@aleksejdix.com>
2020-06-07 21:41:30 +0200

    remove owner policy

--- extensions/users-permissions/config/policies/profileOwner.js
+++
@@ -1,7 +0,0 @@
-module.exports = (ctx, next) => {
-  const userId = +ctx.state.user.id
-  const requestId = +ctx.params.id
-  console.log(userId, requestId)
-  if (userId !== requestId) return ctx.unauthorized('You can only update your own profile')
-  return next()
-}
